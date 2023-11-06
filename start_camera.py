dbfd468dba58fc876d9caf01b740ef5b9447065e
Reza Vilera <vilera_reza99@yahoo.com>
2023-11-06 13:53:29 +0700

    adding face detector

--- start_camera.py
+++ start_camera.py
@@ -250,6 +250,7 @@ async def process_frame(frame_processors=['face']):
 
     while True:
         #try:
+        print (frame)
         frame = await asyncio.to_thread(wait, output)
 
             
